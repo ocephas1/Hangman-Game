4154e32f77ee8bf2d8780bf1da255f497f2c6743
Octavia Cephas <octavia.cephas@gmail.com>
2017-12-31 02:56:38 -0500

    hang man redo, still non functional

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,150 +1,114 @@
-const letter = [ "T", "O", "X", "I", "C"] 
+//letter to guess will be chosen from this array
+
+const letterArray = [ 
+
+["T", "O", "X", "I" , "C"],
+
+["B", "O", "O", "T", "Y", "L" , "I", "C", "I", "O", "U", "S"], 
+
+["B", "U", "R", "N"], 
+
+]
+
+//word chosen will be random 
+
+const random = Math.floor((Math.random()*(letterArray.length-1)));
+
+const letters = letterArray[random];
+
+const chosenWord = new Array(letters.length);
+
+const guesses = 0;
+
+//every letter is represented by an underscore 
+
+for (let i = 0; i < chosenWord.length; i++){
+	chosenWord[i] = "_ ";
+}
+//prints  to the guess field
+
+function printWord() {
+	for (let i = 0; i <
+		chosenWord.length; i++){
+		const div1 = 
+		document.getElementById("div1");
+
+		const wrong = document.createTextNode(chosenWord[i]);
+
+		div1.appendChild(wrong);
+
+	}
+}
+
+//identifies if letter guessed matches one or more letters in the chosen word
+
+const displayGuess = function(){
+	let m = 
+	document.letterGuess;
+	let dm = 
+	m.elements["letterDisplay"]; 
+
+
+
+
+	const display = dm.value;
+	//the letter guessed by player 
+	display =
+
+	display.toUpperCase();
+	//make any letter pressed default to upper case 
+	for (let i = 0; i < letters.length; i ++){
+		if(letters[i] === display) {
+			chosenWord[i] = display + " ";
+
+			const wordMatch = true;
+
+			dm.value = "";
+
+
+	//deletes the guessfield and replaces it with the new one
+	var div1 =
+	document.getElementById("div1");
+		div1.innerHTML="";
+		printDiv1();
+	//if letter guessed is incorrect it willbe places in "incorrect letter list"
+
+	if (!wordMatch){
+
+		const incorrectLetter = document.getElementById("incorrectLetter");
+
+		const incorrect = document.createTextNode(" " + display)
+
+		incorrectLetter.appendChild(incorrect);
+	}
+
+//checks if all letters have been found and word is complete 
+
+	const complete = true; 
+
+		for (let i = 0; i < chosenWord.length; i ++) {
+			if(chosenWord[i] === "_  ") {
+				complete = false;
+
+			}
+		}
+
+		if(complete) {
+			window.alert("You Win!");
+
+		}
+
+//once you get 10 wrong letters you lose 
+		if(guesses === 10) {
+			window.alert("Guess Max -- Game Over");
+
+		}
+	}
 	
-let unguessedLetter = "___"
-let score = 0;
-
-function correctKeyPressed(guess) {
-	correctKeyPressed(letter)
-
-	if ((letter === "t") || (letter ==="o") || (letter ==="x") 
-
-	|| (letter == "i")|| (letter === "c")) {
-
-			document.write(letter);
-
-	score++;
-	updateScore();
-
-}
-	else {
-		prompt("Keep Guessing!")
-	}}
-
-
-	window.addEventListener('keyup', function(guess) {
-
-	})
-
-	"with you", "baby boy", "in da club"]
-
-
-
-	//const userGuess = event.key;
-
-
-	//function renderLetter () {
-
-	//letter = String.fromCharCode(event.keyCode).toLowerCase();
-
-
-	let rightWord = [];
-let wrongWord = [];
-let underScore = [];
-
-//Manipulate html (dom)
-let docUnderScore = document.getElementsByClassName('underScore');
-let docRightGuess = document.getElementsByClassName('rightGuess');
-let docWrongGuess = document.getElementsByClassName('wrongGuess');
-
-//creates underscore based on length of word
-let generateUnderScore = () => {
-	for (let i = 0; i < word.length; i++) {
-		underScore.push('___');
-
-	}
-	return underScore;
+		function gameOver() {
+			printChosenWord();
+		}
+		window.onload = gameOver;
 }
 
-
-document.addEventListener('keypress', (event) => {
-	let keyword = String.fromCharCode(event.keyCode);
-	if(word.indexOf(keyword) > -1){
-		
-		rightWord.push(keyword);
-
-		underScore[word.indexOf(keyword)] = keyword;
-		docUnderScore[0].innerHTML = underScore.join('');
-		docRightGuess[0].innerHTML = rightWord;
-
-	if (underScore.join('') === word) {
-			alert('You Win!');
-		}
-		else {
-
-		wrongWord.push(keyword);
-		docUnderScore[0].innerHTML = wrongGuess.join('');
-		docWrongGuess[0].innerHTML = wrongWord;
-		}
-	}
-
-});
-
-
-
-
-
-
-
-
-//Create array of possible songs 
-
-document.onkeyup = function(event) {
-
-	const userGuess = event.key;
 }
-
-const song = [
-	"toxic",
-	"with you",
-	"baby boy",
-	"in da club"
-
-];
-
-const song = song[Math.floor(Math.random() = song.length)];
-
-let rightWord = [];
-let wrongWord = [];
-let underScore = [];
-
-//Manipulate html (dom)
-let docUnderScore = document.getElementsByClassName('underScore');
-let docRightGuess = document.getElementsByClassName('rightGuess');
-let docWrongGuess = document.getElementsByClassName('wrongGuess');
-
-//creates underscore based on length of word
-let generateUnderScore = () => {
-	for (let i = 0; i < word.length; i++) {
-		underScore.push('___');
-
-	}
-	return underScore;
-}
-
-
-document.addEventListener('keypress', (event) => {
-	let keyword = String.fromCharCode(event.keyCode);
-	if(word.indexOf(keyword) > -1){
-		
-		rightWord.push(keyword);
-
-		underScore[word.indexOf(keyword)] = keyword;
-		docUnderScore[0].innerHTML = underScore.join('');
-		docRightGuess[0].innerHTML = rightWord;
-
-	if (underScore.join('') === word) {
-			alert('You Win!');
-		}
-		else {
-
-		wrongWord.push(keyword);
-		docUnderScore[0].innerHTML = wrongGuess.join('');
-		docWrongGuess[0].innerHTML = wrongWord;
-		}
-	}
-
-});
-
-
-
-
